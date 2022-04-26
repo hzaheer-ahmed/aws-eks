0e8ce2127a3107eb093dd9b32443e4e58dcb4b91
alameen <alameen.info@gmail.com>
2022-04-27 05:17:47 +0530

    updating the infrastructure module

--- rds.tf
+++ rds.tf
@@ -1,73 +1,73 @@
-# module "db8" {
-#   depends_on = [
-#     aws_security_group.mysql
-#   ]
-#   source  = "terraform-aws-modules/rds/aws"
+module "db8" {
+  depends_on = [
+    aws_security_group.mysql
+  ]
+  source = "terraform-aws-modules/rds/aws"
 
-#   identifier = var.name
+  identifier = var.name
 
-#   engine            = var.engine
-#   engine_version    = var.engine_version
-#   instance_class    = var.instance_class
-#   allocated_storage = var.allocated_storage
-#   create_random_password = false
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  allocated_storage      = var.allocated_storage
+  create_random_password = false
 
-#   db_name  = var.db_name
-#   username = var.username
-#   password = var.db_psw
-#   availability_zone = "eu-central-1b"
+  db_name           = var.db_name
+  username          = var.username
+  password          = var.db_psw
+  availability_zone = "eu-central-1b"
 
 
-#   vpc_security_group_ids = [aws_security_group.mysql.id]
+  vpc_security_group_ids = [aws_security_group.mysql.id]
 
-#   create_monitoring_role = false
+  create_monitoring_role = false
 
-#   tags = {
-#     Owner       = "user"
-#     Environment = "test"
-#     name        = var.name
-#     Terraform   = "true"
-#   }
+  tags = {
+    Owner       = "user"
+    Environment = "test"
+    name        = var.name
+    Terraform   = "true"
+  }
 
-#   # DB subnet group
-#   db_subnet_group_name = module.vpc.database_subnet_group_name
-#   # DB parameter group
-#   family = "mysql8.0"
+  # DB subnet group
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+  # DB parameter group
+  family = "mysql8.0"
 
-#   # DB option group
-#   major_engine_version = "8.0"
+  # DB option group
+  major_engine_version = "8.0"
 
-#   # Database Deletion Protection
-#   #deletion_protection = false
+  # Database Deletion Protection
+  #deletion_protection = false
 
-#   parameters = [
-#     {
-#       name = "character_set_client"
-#       value = "utf8mb4"
-#     },
-#     {
-#       name = "character_set_server"
-#       value = "utf8mb4"
-#     }
-#   ]
+  parameters = [
+    {
+      name  = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
 
-#   options = [
-#     {
-#       option_name = "MARIADB_AUDIT_PLUGIN"
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
 
-#       option_settings = [
-#         {
-#           name  = "SERVER_AUDIT_EVENTS"
-#           value = "CONNECT"
-#         },
-#         {
-#           name  = "SERVER_AUDIT_FILE_ROTATIONS"
-#           value = "37"
-#         },
-#       ]
-#     },
-#   ]
-# }
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
 
 module "db5" {
   depends_on = [
